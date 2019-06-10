b8fa40dd15233b602ac84dd0b0f6d6c5b097eceb
luqc <luqcxaj@163.com>
2019-06-10 11:22:29 +0800

    Binding 测试

--- WPFTest/WPFTest/Window1.xaml.cs
+++ WPFTest/WPFTest/Window1.xaml.cs
@@ -19,9 +19,27 @@ namespace WPFTest
     /// </summary>
     public partial class Window1 : Window
     {
+        Student stu;
         public Window1()
         {
             InitializeComponent();
+
+            stu = new Student();
+
+            //准备绑定
+            Binding binding = new Binding();
+            //源
+            binding.Source = stu;
+            binding.Path = new PropertyPath("Name");
+
+            //使用Binding连接数据源和目标
+            BindingOperations.SetBinding(this.textBoxName, TextBox.TextProperty, binding);
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            stu.Name = "Name";
         }
     }
 }
