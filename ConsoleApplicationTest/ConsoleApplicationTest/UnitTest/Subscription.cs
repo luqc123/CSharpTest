5f6f01f2f0adc4b369c868a06a3db1eddaa8b73f
luqc <luqcxaj@163.com>
2019-06-13 19:36:20 +0800

    增加单元测试

---
+++ ConsoleApplicationTest/ConsoleApplicationTest/UnitTest/Subscription.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationTest.UnitTest
+{
+    public class Subscription
+    {
+        public enum Status
+        {
+            Temporary,
+            Financial,
+            Unfinancial,
+            Suspended
+        }
+
+        public DateTime? PaidUpTo { get; set; }
+
+        public Status CurrentStatus
+        {
+            get
+            {
+                if (this.PaidUpTo.HasValue == false)
+                    return Status.Temporary;
+                if (this.PaidUpTo > DateTime.Today)
+                    return Status.Financial;
+                else
+                {
+                    if (this.PaidUpTo >= DateTime.Today.AddMonths(-3))
+                        return Status.Unfinancial;
+                    else
+                        return Status.Suspended;
+                }
+            }
+        }
+    }
+}
