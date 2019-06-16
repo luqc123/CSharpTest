a4eed5e7446e05eac127d6f4447e2c55ccbe9a5d
luqc <luqcxaj@163.com>
2019-06-16 14:49:02 +0800

    TypeConvertTest

---
+++ WPFTest/WPFTest/Human.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFTest
+{
+    [TypeConverter(typeof(StringToHumanConvert))]
+    class Human
+    {
+        public string Name { get; set; }
+        public Human Child { get; set; }
+    }
+
+    public class StringToHumanConvert:TypeConverter
+    {
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (value is string)
+            {
+                Human h = new Human();
+                h.Name = value as string;
+                return h;
+            }
+
+            return base.ConvertFrom(context, culture, value);
+        }
+    }
+}
