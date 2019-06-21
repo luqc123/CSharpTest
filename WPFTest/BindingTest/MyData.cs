23bce8da43797a8f3f81a07a407b5c54a4d7b81e
luqc <luqcxaj@163.com>
2019-06-21 18:09:45 +0800

    DataBinding测试

---
+++ WPFTest/BindingTest/MyData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace BindingTest
+{
+    public class MyData : INotifyPropertyChanged
+    {
+        private DateTime now;
+
+        public MyData()
+        {
+            now = DateTime.Now;
+        }
+        public DateTime Now
+        {
+            get
+            {
+                return now;
+            }
+            set
+            {
+                now = value;
+                OnPropertyChanged("DateTime");
+            }
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(info));
+            }
+        }
+    }
+}
