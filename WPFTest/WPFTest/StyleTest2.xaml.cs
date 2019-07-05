6ae8aa38e88e542579abd2546dea9635f191f3c1
luqc <luqcxaj@163.com>
2019-07-05 18:53:11 +0800

    Style Trigger Test

---
+++ WPFTest/WPFTest/StyleTest2.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFTest
+{
+    /// <summary>
+    /// StyleTest2.xaml 的交互逻辑
+    /// </summary>
+    public partial class StyleTest2 : Window
+    {
+        public StyleTest2()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        public class Student
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Skill { get; set; }
+        }
+        
+        private void Init()
+        {
+            List<Student> info = new List<Student>() {
+                new Student() {Id=1,Name="luqc",Skill="C#"},
+                new Student() {Id=2,Name="pengan",Skill="English"},
+                new Student() {Id=3,Name="xiran",Skill="Excel"},
+            };
+            this.lbInfos.ItemsSource = info;
+        }
+    }
+}
