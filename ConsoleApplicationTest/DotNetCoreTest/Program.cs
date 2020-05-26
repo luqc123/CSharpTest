32b1e268da1d06e38a5e7458f0faaba9ae7e768b
luqc123 <luqcxaj@163.com>
2020-05-27 01:57:10 +0800

    dotnetcore test

---
+++ ConsoleApplicationTest/DotNetCoreTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetCoreTest
+{
+    public class Program
+    {
+        static int Main()
+        {
+            return 0;
+        }
+    }
+}
