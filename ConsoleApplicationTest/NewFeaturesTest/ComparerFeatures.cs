aac4832a7634678ab87a600435d0c27144fa805f
luqc <luqcxaj@163.com>
2020-05-27 11:51:50 +0800

    Comparer test

---
+++ ConsoleApplicationTest/NewFeaturesTest/ComparerFeatures.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static NewFeaturesTest.ClassFeatures;
+
+namespace NewFeaturesTest
+{
+    public class ComparerFeatures
+    {
+        //c# 1 
+        public class ProductComparerVersionOne : IComparer
+        {
+            public int Compare(object x, object y)
+            {
+                ProductVersionOne p1 = (ProductVersionOne)x;
+                ProductVersionOne p2 = (ProductVersionOne)y;
+                return p1.Name.CompareTo(p2.Name);
+            }
+        }
+
+        //c# 2 generic
+        public class ProductComparerVersionTwo : IComparer<ProductVersionTwo>
+        {
+            public int Compare(ProductVersionTwo x, ProductVersionTwo y)
+            {
+                return x.Name.CompareTo(y.Name);
+            }
+        }
+    }
+}
--- ConsoleApplicationTest/NewFeaturesTest/Program.cs
+++ ConsoleApplicationTest/NewFeaturesTest/Program.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static NewFeaturesTest.ComparerFeatures;
+using static NewFeaturesTest.ClassFeatures;
 
 namespace NewFeaturesTest
 {
@@ -12,6 +14,7 @@ namespace NewFeaturesTest
         static void Main()
         {
             ArrayList product1 = ClassFeatures.ProductVersionOne.GetSomeProduct();
+            product1.Sort(new ProductComparerVersionOne());
             foreach (var t in product1)
             {
                 Console.WriteLine(t);
@@ -19,18 +22,25 @@ namespace NewFeaturesTest
 
             //generic no boxing
             List<ClassFeatures.ProductVersionTwo> product2 = ClassFeatures.ProductVersionTwo.GetSomeProduct();
+            //product2.Sort(new ProductComparerVersionTwo());
+            product2.Sort(delegate (ProductVersionTwo x, ProductVersionTwo y) { return x.Name.CompareTo(y.Name); });
             foreach(var item in product2)
             {
                 Console.WriteLine(item);
             }
 
             var product3 = ClassFeatures.ProductVersionThree.GetSomeProduct();
+            product3.Sort((x, y) => x.Name.CompareTo(y.Name));
             foreach(var p in product3)
             {
                 Console.WriteLine(p);
             }
 
             var product4 = ClassFeatures.ProductVersionFour.GetSomeProduct();
+            foreach(var p in product4.OrderBy(o=>o.Name))
+            {
+                Console.WriteLine(p);
+            }
             foreach(var p in product4)
             {
                 Console.WriteLine(p);
